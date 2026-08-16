841015f05aa6b06ce191cd0dc5fd7ca36315b702
GTnoBRAND <jaskaamanbayev2003@gmail.com>
2026-08-16 22:48:16 +0500

    Parametrized constructor reference explanation with

---
+++ impl/src/main/java/OOP/CopyConstructor.java
@@ -0,0 +1,12 @@
+package OOP;
+
+
+/**
+ * Copy constructor main use case is copying data of the one object s field values and creating a new copy that do not share the same memory location
+ * and change in one object does not effect in another with primitive data fields and it`s values such as int, float, etc.*/
+public class CopyConstructor {
+
+    static void main() {
+
+    }
+}
