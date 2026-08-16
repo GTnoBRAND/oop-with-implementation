bbeba9e0dfaa79cd79a97d9b85a7830656372aa0
GTnoBRAND <jaskaamanbayev2003@gmail.com>
2026-08-16 13:18:22 +0500

    Default constructor reference explanation with simple code  implementation of it.

---
+++ impl/src/main/java/OOP/DefaultConstructor.java
@@ -0,0 +1,22 @@
+package OOP;
+
+
+/**
+ * Default constructor is implmented automatically.
+ * As it`s name states, default constructor has no parameters, it is used to assign values to an object*/
+public class DefaultConstructor {
+
+
+    public DefaultConstructor(){
+        //default with no parameters
+        System.out.println("Default constructor");
+    }
+
+
+
+    static void main() {
+
+        DefaultConstructor defaultConstructor = new DefaultConstructor();
+
+    }
+}
